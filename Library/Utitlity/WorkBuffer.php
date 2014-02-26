f7fdceed01443e01734d1ee071f69a41b1311678
lemon-lime <josh@rdrkt.com>
2014-02-27 10:03:54 +1100

    First attempt

--- Library/Utitlity/WorkBuffer.php
+++ Library/Utitlity/WorkBuffer.php
@@ -2,6 +2,13 @@
 namespace Utility;
 class WorkBuffer {
 	private $_redis;
+	private $_prefix = 'wub';
+	
+	private function _prefix_key(){
+		$args = func_get_args();
+		array_unshift($args, $this->_prefix);
+		return implode('-', $args);
+	}
 	
 	public function __construct(Redis $redis){
 		$this->setRedis($redis);
@@ -12,20 +19,41 @@ class WorkBuffer {
 	}
 	
 	public function buffer(WorkItem $work){
-		$key = $work->getBufferName();
-		$value = (string) $work;
-		$priority = $work->getPriority();
+		$queue = $this->_prefixKey($work->getBufferName(), 'queue');
 		
-		$this->_redis->zAdd($key, $value, $priority);
+		$id = $work->getId();
+		$this->setWorkData($key, $id, $work);
+		
+		//store data before queueing
+		return $this->_redis->lPush($queue, $id); //'beginning' of the list is actually the last to process
 	}
 	
 	public function hasNext($key){
-		return ($this->_redis->zCard($key) > 0);
+		$key = $this->_prefixKey($key, 'queue');
+		return ($this->_redis->lLen($key) > 0);
 	}
 	
-	public function getNext($key){
-		$item = $this->redis->zRevRange($key, 0, 0);
-		$this->redis->zRem($item);
-		return new WorkItem($item);
+	public function getNext($key, $timeout = 1000){
+		$queue = $this->_prefixKey($key, 'queue' );
+		$process = $this->_prefixKey($key, 'process');
+		
+		$id = $this->redis->bRPopLPush($queue, $process, $timeout);
+		
+		if($id){
+			return $this->_getWorkItem($key, $id);
+		}
+		
+		return false;
+	}
+	
+	private function _setWorkItem($key, $id, $work){
+		$keyData = $this->_prefixKey($key, 'data');
+		
+		$value = serialize($work);
+		return $this->_redis->hSet($keyData, $id, $value);
+	}
+	private function _getWorkItem($key, $id){
+		$keyData = $this->_prefixKey($key, 'data');
+		return unserialize($this->_redis->hGet($keyData, $id));
 	}
 }
